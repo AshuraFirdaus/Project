cdb547eba073668d97de8df5e18f9c34dfb5417b
Muhamad Firdaus <70957708+AshuraFirdaus@users.noreply.github.com>
2023-03-13 23:07:58 +0700

    Add files via upload

---
+++ machinelearning/project/project5/pizzadataset/UAS2A_2.py
@@ -0,0 +1,53 @@
+from sklearn import datasets
+import pandas as pd
+from scipy.spatial.distance import cdist, pdist
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
+
+Pizza = pd.read_csv('Pizza3.csv')
+PizzaDataset = pd.read_csv('Pizza3.csv', names=[
+                           'mois', 'prot', 'fat', 'ash', 'sodium', 'crab', 'cal'], header=0)
+
+Pizza.columns = Pizza.columns.str.replace(' ', '')
+Pizza.head()
+X = Pizza.iloc[:, :3]
+
+sc = StandardScaler()
+sc.fit(X)
+X = sc.transform(X)
+K = range(1, 10)
+KM = [KMeans(n_clusters=k).fit(X) for k in K]
+centroids = [k.cluster_centers_ for k in KM]
+D_k = [cdist(X, cent, 'euclidean') for cent in centroids]
+cIdx = [np.argmin(D, axis=1) for D in D_k]
+dist = [np.min(D, axis=1) for D in D_k]
+avgWithinSS = [sum(d)/X.shape[0] for d in dist]
+
+wcss = [sum(d**2) for d in dist]
+tss = sum(pdist(X)**2)/X.shape[0]
+bss = tss-wcss
+varExplained = bss/tss*100
+kIdx = 10-1
+
+kIdx = 2
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.plot(K, avgWithinSS, 'b*-')
+plt.plot(K[kIdx], avgWithinSS[kIdx], marker='o', markersize=12,
+         markeredgewidth=2, markeredgecolor='r', markerfacecolor='None')
+plt.grid(True)
+plt.xlabel('Number of clusters')
+plt.ylabel('Average within-cluster sum of squares')
+plt.title('Elbow for KMeans clustering')
+plt.subplot(1, 2, 2)
+plt.plot(K, varExplained, 'b*-')
+plt.plot(K[kIdx], varExplained[kIdx], marker='o', markersize=12,
+         markeredgewidth=2, markeredgecolor='r', markerfacecolor='None')
+plt.grid(True)
+plt.xlabel('Number of clusters')
+plt.ylabel('Percentage of variance explained')
+plt.title('Elbow for KMeans clustering')
+plt.tight_layout()
+plt.show()
